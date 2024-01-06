bcfc9960e5b6de51594ae91c029c3aa48d29de7b
Walson Li <walsonli@Temp.cable.rcn.com>
2024-01-06 16:52:19 -0500

    added advanced search, needs ui improvements

--- previous/v1.1/assets/js/main.js
+++ previous/v1.1/assets/js/main.js
@@ -1,21 +1,36 @@
+/**
+ * Choosing Pages
+ */
+
 const statisticsPage = document.getElementById('statistics-page');
 const inputPage = document.getElementById('input-page');
 const searchPage = document.getElementById('search-page');
 
 function showStatisticsPage() {
-    statisticsPage.style.visibility = 'visible';
-    inputPage.style.visibility = 'hidden';
-    searchPage.style.visibility = 'hidden';
+    statisticsPage.style.display = 'block';
+    inputPage.style.display = 'none';
+    searchPage.style.display = 'none';
 }
 
 function showInputPage() {
-    statisticsPage.style.visibility = 'hidden';
-    inputPage.style.visibility = 'visible';
-    searchPage.style.visibility = 'hidden';
+    statisticsPage.style.display = 'none';
+    inputPage.style.display = 'block';
+    searchPage.style.display = 'none';
 }
 
 function showSearchPage() {
-    statisticsPage.style.visibility = 'hidden';
-    inputPage.style.visibility = 'hidden';
-    searchPage.style.visibility = 'visible';
+    statisticsPage.style.display = 'none';
+    inputPage.style.display = 'none';
+    searchPage.style.display = 'block';
+}
+
+/**
+ * Toggle Searching
+ */
+const normalSearch = document.getElementById('normal-search');
+const advancedSearch = document.getElementById('advanced-search');
+
+function toggleAdvancedSearch(){
+    advancedSearch.style.display = (advancedSearch.style.display === 'none' || advancedSearch.style.display === '') ? 'block' : 'none';
+    normalSearch.style.display = (advancedSearch.style.display === 'none' || advancedSearch.style.display === '') ? 'block' : 'none';
 }
