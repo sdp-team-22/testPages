e2aab1f344fbada3cf3307547bac6c596301bd9a
PetroFilip <116189942+PetroFilip@users.noreply.github.com>
2024-04-04 16:17:59 -0400

    Update stats.service.ts

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -8,7 +8,7 @@ export class StatsService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    let url = 'http://127.0.0.1:5000/api/data';
+    let url = '/api/data';
     return this.http.get(url);
   }
 
