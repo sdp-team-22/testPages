353cf7ed3b1fc74ef55bd2d8826ddfb67eebe19a
John Bogacz <Wantt0c0de912@tutanota.com>
2024-04-04 15:51:15 -0400

    hi

--- flask_db_conn_api/app.py
+++ flask_db_conn_api/app.py
@@ -209,6 +209,9 @@ def database_upload():
     
     return jsonify("success")
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port= 5000,
+        debug=True
+    )
